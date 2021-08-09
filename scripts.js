18d1b63723d1a17283e47feed78928094676b2bc
NIck <nicholasbenzinger@gmail.com>
2021-08-09 13:11:02 -0700

    adds liter to gallon coversion
    co-authored by Chynna Lew chynnalew@yahoo.com
    co-authored by Chynna Lew chynnalew@yahoo.com

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const liter = prompt("How many liters?");
+const number1 = liter;
+const number2 = 3.785;
+function gallon(number1, number2){
+return number2 / number1;
+}
+alert(gallon(number1, number2));
